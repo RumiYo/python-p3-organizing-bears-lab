48c85b8c76c7eb6183fe99115d9bc49058b1ee06
RumiYo <yokoyamarumi@gmail.com>
2024-03-28 12:59:24 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'Black','Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'Brown', 'Phlegmatic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'Purple', 'Meranchonic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'Dark brown', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Pink', 'Meranchonic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'White','Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'Blue', 'Phlegmatic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( NULL, 5, 'F', 'Green', 'Sanguine', 1);
